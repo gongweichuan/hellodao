2a87525f01280d5b2e30012f3c9cb2b74717da0a
gongweichuan <gwc@windows10.microdone.cn>
2019-09-19 04:08:49 +0800

    增加requstMapping注解

--- hellodao/src/main/java/net/gwc/springboot/hellodao/HellodaoApplication.java
+++ hellodao/src/main/java/net/gwc/springboot/hellodao/HellodaoApplication.java
@@ -2,12 +2,20 @@ package net.gwc.springboot.hellodao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HellodaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellodaoApplication.class, args);
 	}
 
+	@RequestMapping("")
+	public String hello()
+	{
+		return "Hello World!";
+	}
 }
