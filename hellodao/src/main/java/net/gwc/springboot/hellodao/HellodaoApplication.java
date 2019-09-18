310dd2a1d92182ae2beafbfb8d907fee37fc8f3f
gongweichuan <gwc4reg@outlook.com>
2019-09-19 03:59:59 +0800

    初始提交

---
+++ hellodao/src/main/java/net/gwc/springboot/hellodao/HellodaoApplication.java
@@ -0,0 +1,13 @@
+package net.gwc.springboot.hellodao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellodaoApplication.class, args);
+	}
+
+}
